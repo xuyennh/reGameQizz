057b2369b922ea7d5ff0d7d2e2ed62df8b38dc22
xuyen99bn <xuyenhuynguyen@gmail.com>
2020-07-14 10:55:43 +0700

    first

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -3676,7 +3676,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64565" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54643" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
